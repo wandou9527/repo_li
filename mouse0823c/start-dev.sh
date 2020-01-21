6c60dc3a56ff8da1b364047ab01c67de575e3cc5
liming <liming@ithro.com>
2020-01-21 16:34:05 +0800

    启动脚本

---
+++ mouse0823c/start-dev.sh
@@ -0,0 +1,36 @@
+echo =================================
+psid=0
+javaps=`jps -l | grep mouse0823c`
+ if [ -n "$javaps" ]; then
+    psid=`echo $javaps | awk '{print $1}'`
+ else
+    psid=0
+ fi
+ if [ $psid -ne 0 ]; then
+    echo -n "Stopping mouse0823clala ...(pid=$psid) "
+    `kill -9 $psid`
+    if [ $? -eq 0 ]; then
+       echo "[OK]"
+    else
+       echo "[Failed]"
+    fi
+ else
+    echo "================================"
+    echo "info: is not running"
+    echo "================================"
+ fi
+echo '准备自动启动「mouse」服务'
+cd /Users/liming/Documents/java/java_self/git_site_self/repo_li/mouse0823c
+echo 'git pull ...'
+git pull
+echo 'mvn clean package ...'
+mvn clean package -Dmaven.test.skip=true
+echo '进入 target 目录'
+cd /Users/liming/Documents/java/java_self/git_site_self/repo_li/mouse0823c/target
+echo 'run java -jar 。。。'
+nohup java -XX:ParallelGCThreads=2 -Djava.compiler=NONE -jar mouse0823c-1.1.8.jar --spring.profiles.active=dev &
+sleep 5
+tail -f nohup.out
+sleep 30
+exit 0
+
