7cfbd414221d5a044efcee120655b778b66db4f9
liming <liming@ithro.com>
2020-03-26 12:10:18 +0800

    echarts 动态数据 二维数据

--- mouse0823c/src/main/java/com/wandou/controller/MatterLogController.java
+++ mouse0823c/src/main/java/com/wandou/controller/MatterLogController.java
@@ -1,9 +1,11 @@
 package com.wandou.controller;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -20,15 +22,18 @@ public class MatterLogController {
     @GetMapping("/list")
     public List<Object> list() {
         List<Object> matterLogs = new LinkedList<>();
-        matterLogs.add("2020-03-16");
-        matterLogs.add(6);
-        matterLogs.add("2020-03-17");
-        matterLogs.add(5);
-        matterLogs.add("2020-03-18");
-        matterLogs.add(1);
-        matterLogs.add("2020-03-25");
-        matterLogs.add(5.5);
+        addList(matterLogs, "2020-03-16", 6);
+        addList(matterLogs, "2020-03-17", 5);
+        addList(matterLogs, "2020-03-18", 1);
+        addList(matterLogs, "2020-03-19", 5.5);
         return matterLogs;
+    }
+
+    /**
+     * 二维数组 list里加list
+     */
+    private void addList(List<Object> list, Object var1, Object var2) {
+        list.add(Arrays.asList(var1, var2));
     }
 
 }
