56c0bb5694915e53789ff32b016965849a60c0d1
liming <liming@ithro.com>
2020-04-03 22:46:14 +0800

    换用户 基本ok

--- fe-demo/config/config.js
+++ fe-demo/config/config.js
@@ -1,5 +1,12 @@
 const mHost = 'http://wandour.top/';
 const listMatterLogUrl = mHost + '/mouse/matter_log/list';
+const list = ['1', '2', 'str3', 'str4', "str5", "str6", "str7"];
+const confUsers = [
+    {id: 1, nick: '李明'},
+    {id: 2, nick: '艳杰'},
+    {id: 23, nick: '知足是福'},
+    {id: 25, nick: '阿冷'}
+]
 
 
 
